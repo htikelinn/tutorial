605dd45aa720ba190dcc603b56e28e41ddc63d9e
htikelinn <htikezawlinn@gmail.com>
2025-06-15 18:44:15 +0630

    create folder move basic learning java

---
+++ basicjava/InstanceMethodDemo.java
@@ -0,0 +1,22 @@
+public class InstanceMethodDemo {
+
+    private String format = "Hello %s%n";
+
+    public void showMessage(String name) {
+        System.out.format(format, name);
+    }
+
+    public void showMessage(String[] names) {
+        for (String name : names) {
+            showMessage(name);
+        }
+    }
+
+    public static void main(String[] args) {
+        InstanceMethodDemo demo = new InstanceMethodDemo();
+        demo.showMessage("java");
+
+        String[] array = { "Java", "Programming" };
+        demo.showMessage(array);
+    }
+}
